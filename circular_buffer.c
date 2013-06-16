22cd4da274b22c7c1a20fae5c59eb2417bee4a59
Erich Schroeter <erichschroeter@gmail.com>
2013-06-15 21:59:32 -0500

    Removed dependency on dbg.h; allows for drop-in portability.

--- circular_buffer.c
+++ circular_buffer.c
@@ -4,8 +4,13 @@
 #include <errno.h>
 #include <unistd.h>
 
-#include "dbg.h"
 #include "circular_buffer.h"
+
+#ifdef DEBUG
+#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#else
+#define debug(M, ...)
+#endif
 
 struct circular_buffer *circular_buffer_create(int length)
 {
